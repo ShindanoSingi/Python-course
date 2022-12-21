2ba5d2b4723387e9581209a0304f48239fd4fffc
ShindanoSingi <shindanosingi@sekur.com>
2022-12-21 00:23:07 -0500

    completed functions

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -1,21 +1,44 @@
 #
 # Example file for working with functions
-# LinkedIn Learning Python course by Joe Marini
 #
 
 
 # TODO: define a basic function
+def func1():
+      print("I am a function one.")
+
+# func1()
+# print(func1())
+# print(func1)
 
 
 # TODO: function that takes arguments
+def func2(arg1, arg2):
+      print(arg1, "", arg2)
 
+# func2(3,5)
 
 # TODO: function that returns a value
+def func3(x):
+      return x*x*x
 
+# print(func3(9))
 
 # TODO: function with default value for an argument
+def power(num, x=1):
+      result = 1;
+      for i in range(x):
+            result = result * num;
+      return result;
 
+# print(power(2))
+# print(power(2, 2))
 
 # TODO: function with variable number of arguments
+def multiple_add(*args):
+      result = 0;
+      for item in args:
+            result+=item
+      return result
 
-
+print(multiple_add(4,5,10,4, 10))
