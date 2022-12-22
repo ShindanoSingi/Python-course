c12c569e5dc60897cab8fac2a9484fcf868e34bc
ShindanoSingi <shindanosingi@sekur.com>
2022-12-22 01:43:53 -0500

    completed loops

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -1,19 +1,42 @@
 #
 # Example file for working with conditional statements
-# LinkedIn Learning Python course by Joe Marini
 #
 
 
 
 def main():
-    x, y = 10, 100
+    x, y = 100, 100
 
     # conditional flow uses if, elif, else
+    if x < y:
+        result = "x is less than y."
+        print(result)
 
-    # conditional statements let you use "a if C else b"
+    elif x == y:
+        result = "x is equal to y."
+        print(result);
 
-    # match-case makes it easy to compare multiple values
-    value = "one"
+    else:
+        result = "x is greater than y."
+        print(result)
+# main()
 
-if __name__ == "__main__":
-    main()
+# conditional statements let you use "a if C else b"
+
+# match-case makes it easy to compare multiple values
+
+value = "4"
+match value:
+    case "one":
+        result = 1
+    case "two":
+        result = 2
+    case "four":
+        result = 4
+    case _:
+        result = -1
+print(result)
+# value = "one"
+# if __name__ == "__main__":
+#     main()
+
