d0d2337c865c3941a9464e07c37de6f0731f94e9
ShindanoSingi <shindanosingi@sekur.com>
2022-12-22 20:08:18 -0500

    c modules

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -1,14 +1,12 @@
-# LinkedIn Learning Python course by Joe Marini
-#
-
 
 # TODO: import the math module, which contains features for working with mathematics
-
+import  math
 
 # TODO: the math module contains lots of pre-built functions
+print("The square root of 16 is ", int(math.sqrt(16)),".")
 
-
-# TODO: in addition to functions, some modules contain useful constants 
-
+# TODO: in addition to functions, some modules contain useful constants
+print("Pi is ", "{:.2f}".format(math.pi))
 
 # TODO: try some of the math functions for yourself here:
+print("3 to the power of 3 is", "{:.0f}".format(math.pow(3,3)))
