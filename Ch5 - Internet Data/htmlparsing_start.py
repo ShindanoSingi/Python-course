698a4ea72ff99a5397ebc3886090ad4ebc93bec9
ShindanoSingi <shindanosingi@sekur.com>
2022-12-24 14:24:55 -0500

    c sample html

--- Ch5 - Internet Data/htmlparsing_start.py
+++ Ch5 - Internet Data/htmlparsing_start.py
@@ -1,29 +1,34 @@
-# 
-# Example file for parsing and processing HTML
-# LinkedIn Learning Python course by Joe Marini
 #
+# Example file for parsing and processing HTML
 
 from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
     def handle_comment(self, data):
-        pass
+        print("Encountered a comment:", data)
+        pos = self.getpos()
+        print("At line:", pos[0]," position", pos[1])
 
     def handle_starttag(self, tag, attrs):
-        pass
+        print("Encountered a start tag:", tag)
+        pos = self.getpos()
+        print("At line:", pos[0]," position", pos[1])
 
     def handle_data(self, data):
-        pass
+        if data.isspace:
+            return
+        print("Encountered text data:", data)
+        pos = self.getpos()
+        print("At line:", pos[0]," position", pos[1])
 
 def main():
     # instantiate the parser and feed it some HTML
     parser = MyHTMLParser()
-    
+
     f = open("samplehtml.html")
     if f.mode == "r":
         contents = f.read() # read the entire file
-        parser.feed(contents)    
+        parser.feed(contents)
 
 if __name__ == "__main__":
     main()
-  
